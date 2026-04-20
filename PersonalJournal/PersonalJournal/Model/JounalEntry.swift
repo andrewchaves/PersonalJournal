50459bce4281801ad43877c2d573de7ea98ce8a1
Andrew Chaves <andrew.almeidachaves@gmail.com>
2026-04-20 15:31:42 -0300

    Create add memory form view

---
+++ PersonalJournal/PersonalJournal/Model/JounalEntry.swift
@@ -0,0 +1,14 @@
+//
+//  JounalEntry.swift
+//  PersonalJournal
+//
+//  Created by Andrew Vale on 17/04/26.
+//
+
+import Foundation
+
+struct JournalEntry {
+    var title: String
+    var content: String
+    var timestamp: Date
+}
--- PersonalJournal/PersonalJournal/View/AddNewMemoryView.swift
+++ PersonalJournal/PersonalJournal/View/AddNewMemoryView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct AddNewMemoryView: View {
+    
+    @State private var title: String = ""
+    @State private var content: String = ""
+    @State private var timestamp: Date = Date()
+    
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        VStack {
-            Text("Add New Item")
+        NavigationView {
+            ZStack {
+                
+                Color(.mint)
+                    .ignoresSafeArea()
+                
+                Form {
+                    
+                    Section(header: Text("Informações")) {
+                        TextField("Título", text: $title)
+                        
+                        DatePicker(
+                            "Data",
+                            selection: $timestamp,
+                            displayedComponents: [.date, .hourAndMinute]
+                        )
+                    }
+                    .listRowBackground(Color.formSection)
+                    
+                    Section(header: Text("Conteúdo")) {
+                        TextEditor(text: $content)
+                            .frame(minHeight: 150)
+                    }
+                    .listRowBackground(Color.formSection)
+                    
+                    Section {
+                        Button(action: {
+                            print("Clicou em salvar!")
+                        }) {
+                            Text("Salvar")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                                .foregroundColor(.black)
+                                .fontWeight(.medium)
+                        }
+                        .disabled(title.isEmpty || content.isEmpty)
+                    }
+                    .listRowBackground(Color.button)
+                }
+                .navigationTitle("O que houve hoje?")
+                .scrollContentBackground(.hidden)
+            }
         }
-        .padding()
     }
 }
 
